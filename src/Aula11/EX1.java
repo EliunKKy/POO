51fae0daa004afd088c7013b8a3457228a091da5
Rodrigo Azevedo <102792768+EliunKKy@users.noreply.github.com>
2022-06-08 15:01:28 +0100

    Aula 11
    Update
    08/06/2022
    15:00

--- src/Aula11/EX1.java
+++ src/Aula11/EX1.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,14 +13,34 @@ public class EX1 {
     public static void main(String[] args) throws IOException{
         Scanner input = new Scanner(new FileReader("words.txt"));
         Map<String, HashMap<String, Integer>> par = new HashMap<>();
-        Map<String, Integer> oc = new HashMap<>();
         List<String> lst = new ArrayList<>();
 
         while(input.hasNext()){
             String word = input.next();
-            lst.add(word);
+            
+            if(word.length() > 2){
+                lst.add(word);
+            }
         }
 
+        System.out.println(lst);
+
+        String st = null;
+        for (int i = 0; i < lst.size(); i++) {
+            if(i == 0){ 
+                st = lst.get(i);
+                continue;
+            }
+
+            Map<String, String> oc = new HashMap<>();
+            oc.put(lst(i), 1);
+            par.put(st, oc);
+                
+
+            st = lst.get(i);
+        }
+        
+        
         
     }
 }
