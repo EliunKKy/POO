52e6691e31985e14283632c167c4ab669cb1b714
Rodrigo Azevedo <102792768+EliunKKy@users.noreply.github.com>
2022-06-13 10:52:41 +0100

    Update EX1.java
    Aula11 updates
    31/06/2022
    10:52

--- src/Aula11/EX1.java
+++ src/Aula11/EX1.java
@@ -4,42 +4,49 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class EX1 {
     public static void main(String[] args) throws IOException{
         Scanner input = new Scanner(new FileReader("words.txt"));
-        Map<String, HashMap<String, Integer>> par = new HashMap<>();
-        List<String> lst = new ArrayList<>();
+        List<String> lst = new ArrayList<String>();           
 
+    
         while(input.hasNext()){
-            String word = input.next();
             
+            String word = input.next();
             if(word.length() > 2){
                 lst.add(word);
             }
         }
 
-        System.out.println(lst);
+        System.out.println(lst + "\n");
 
-        String st = null;
-        for (int i = 0; i < lst.size(); i++) {
-            if(i == 0){ 
-                st = lst.get(i);
-                continue;
+        for(int i = 0; i < lst.size(); i++){
+            Map<String, Integer> oc = new HashMap<>();
+            String key = lst.get(i);
+
+            if(i == 0) continue;
+            String s = key + lst.get(i-1);
+
+            oc.put(key, 0);
+
+            for (int n = 0; n < lst.size(); n++){
+
+                if(n == 0) {continue;}
+                String st = lst.get(n) + lst.get(n-1);
+
+                if(st.equals(s)){
+                    oc.put(key, oc.get(key) + 1);
+                }
             }
 
-            Map<String, String> oc = new HashMap<>();
-            oc.put(lst(i), 1);
-            par.put(st, oc);
-                
-
-            st = lst.get(i);
+            System.out.println(lst.get(i-1) + " = " + oc);
         }
-        
         
         
     }
